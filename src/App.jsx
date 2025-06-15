3522653b4594c1bd22464656950795f333fc71b5
Mingkli <leeereece@gmail.com>
2025-06-15 22:42:28 +1000

    Fix router for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import MyProject from './pages/MyProject';
 import AboutMe from './pages/AboutMe';
